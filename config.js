2a5e4c59ec736ab5f4469569d75be8e1307adbfa
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-11 02:46:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x09ee2487f8a70d42f9Ec483df42aB5168A86a5e0",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/pons_applestore",
   NAME:    "Pons Apple Store",
   TICKER:  "$PAP",
